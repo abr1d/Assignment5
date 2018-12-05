66b7945b5cde1da4cc333b4fd915a507efaad21c
abr1d <42777867+abr1d@users.noreply.github.com>
2018-12-05 15:19:43 -0500

    Update s_install.sh

--- s_install.sh
+++ s_install.sh
@@ -1,30 +1,35 @@
 #install mariadb
-sudo yum install mariadb-server mariadb-devel -y
+
+#sudo yum install mariadb-server mariadb-devel -y
 
 # global users
-sudo export MUNGEUSER=991
-sudo groupadd -g $MUNGEUSER munge
-sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
-sudo export SLURMUSER=992
-sudo groupadd -g $SLURMUSER slurm
-sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
+
+#sudo export MUNGEUSER=991
+#sudo groupadd -g $MUNGEUSER munge
+#sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
+#sudo export SLURMUSER=992
+#sudo groupadd -g $SLURMUSER slurm
+#sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
 
 #install munge
-sudo yum install epel-release -y
-sudo yum install munge munge-libs munge-devel -y
+
+#sudo yum install epel-release -y
+#sudo yum install munge munge-libs munge-devel -y
 
 #install rng tools, create a key
-sudo yum install rng-tools -y
-sudo rngd -r /dev/urandom
+
+#sudo yum install rng-tools -y
+#sudo rngd -r /dev/urandom
 
 #stop firewall
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
 
-firewall-cmd --permanent --zone=public --add-port=6817/udp
-firewall-cmd --permanent --zone=public --add-port=6817/tcp
-firewall-cmd --permanent --zone=public --add-port=6818/tcp
-firewall-cmd --permanent --zone=public --add-port=6818/tcp
-firewall-cmd --permanent --zone=public --add-port=7321/tcp
-firewall-cmd --permanent --zone=public --add-port=7321/tcp
-firewall-cmd --reload
+#sudo systemctl stop firewalld
+#sudo systemctl disable firewalld
+
+#firewall-cmd --permanent --zone=public --add-port=6817/udp
+#firewall-cmd --permanent --zone=public --add-port=6817/tcp
+#firewall-cmd --permanent --zone=public --add-port=6818/tcp
+#firewall-cmd --permanent --zone=public --add-port=6818/tcp
+#firewall-cmd --permanent --zone=public --add-port=7321/tcp
+#firewall-cmd --permanent --zone=public --add-port=7321/tcp
+#firewall-cmd --reload
